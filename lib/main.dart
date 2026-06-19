6638fe399178b4f68a13adc1d7d3943cfa8d2c39
JohnnyGZ41 <johnnyundertale41@outlook.com>
2026-06-18 22:49:01 -0500

    feat() update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:power_sense/core/di/dependency_injection.dart';
+import 'package:power_sense/features/auth/presentation/login_page.dart';
+import 'package:power_sense/features/auth/presentation/login_view_model.dart';
 import 'package:provider/provider.dart';
-import 'features/main/presentation/main_page.dart';
+
 
 void main() {
-  
-  runApp(const MainApp());
+  setupDependencies();
+  runApp(
+    MultiProvider(
+    providers: [
+      BlocProvider(create: (context)=> getIt<LoginViewModel>()),
+    ],
+    child: MainApp(),
+    ) 
+  );
 }
 
 class MainApp extends StatelessWidget {
@@ -13,7 +24,8 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MainPage(),
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
     );
   }
 }
