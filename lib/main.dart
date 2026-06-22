48532f0b80b989baf05b3db2e83eace1e3ba90a6
JohnnyGZ41 <johnnyundertale41@outlook.com>
2026-06-21 22:28:17 -0500

    update(): add getIt to RegisterViewModel

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:power_sense/core/di/dependency_injection.dart';
 import 'package:power_sense/features/auth/presentation/login_page.dart';
 import 'package:power_sense/features/auth/presentation/login_view_model.dart';
+import 'package:power_sense/features/auth/presentation/register_view_model.dart';
 import 'package:provider/provider.dart';
 
 
@@ -12,6 +13,7 @@ void main() {
     MultiProvider(
     providers: [
       BlocProvider(create: (context)=> getIt<LoginViewModel>()),
+      BlocProvider(create: (context) => getIt<RegisterViewModel>()),
     ],
     child: MainApp(),
     ) 
